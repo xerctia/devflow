8ff6661884022fc524fd8bbf463fda555d1dd760
swarup Goswami <goswamiswarup97@gmail.com>
2024-11-29 20:18:38 +0530

    removing the comments

--- backend/models/presentation.js
+++ backend/models/presentation.js
@@ -1,42 +1,4 @@
-// // models/Presentation.js
-// import mongoose from 'mongoose';
 
-// const slideSchema = new mongoose.Schema({
-//   slideNumber: Number,
-//   elements: [{
-//     type: {
-//       type: String,
-//       required: true
-//     },
-//     content: String,
-//     style: {
-//       x: Number,
-//       y: Number,
-//       width: Number,
-//       height: Number,
-//       color: String,
-//       backgroundColor: String,
-//       fontSize: Number,
-//     }
-//   }]
-// });
-
-// const presentationSchema = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//     default: 'Untitled Presentation'
-//   },
-//   slides: [slideSchema],
-//   lastModified: {
-//     type: Date,
-//     default: Date.now
-//   }
-// }, {
-//   timestamps: true
-// });
-
-// export const Presentation = mongoose.model('Presentation', presentationSchema);
 
 
 
